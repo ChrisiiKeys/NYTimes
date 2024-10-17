bde2f8000d84aff007662644476822c4bd1ad15c
ChrisiiKeys <k.christopher@nagarro.com>
2024-10-17 15:24:38 +0530

    Add files via upload

---
+++ NewYorkTimesProject/src/test/java/org/runner/TestRunner.java
@@ -0,0 +1,25 @@
+package org.runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.core.snippets.SnippetType;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions( 
+		features = "src//test//resources//",
+		glue = "org.stepdefinition",
+		monochrome = true,
+		dryRun = false,
+		plugin = {"pretty", "html:src\\test\\resources\\Reports\\HtmlReport",
+				"json:src\\test\\resources\\Reports\\report.json",
+				"junit:src\\test\\resources\\Reports\\Cucumber.xml"}
+		
+		
+		
+		
+		)
+public class TestRunner {
+
+}
